ecd00b2cc4dbabdd349d04b75024974e575f390a
robertlyc <merlinlyc@gmail.com>
2014-08-26 16:44:27 +0800

    add coredata support

---
+++ MyLocations/Location.h
@@ -0,0 +1,22 @@
+//
+//  Location.h
+//  MyLocations
+//
+//  Created by RoBeRt on 14-8-26.
+//  Copyright (c) 2014年 SpringShine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Location : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longtitude;
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSString * locationDesctription;
+@property (nonatomic, retain) NSString * category;
+@property (nonatomic, retain) CLPlacemark *placemark;
+
+@end
---
+++ MyLocations/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  MyLocations
+//
+//  Created by RoBeRt on 14-8-26.
+//  Copyright (c) 2014年 SpringShine. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@dynamic latitude;
+@dynamic longtitude;
+@dynamic date;
+@dynamic locationDesctription;
+@dynamic category;
+@dynamic placemark;
+
+@end
