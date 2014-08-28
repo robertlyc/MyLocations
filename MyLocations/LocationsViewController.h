d8c2d1061700aa3d54a4c05db6f65da9d8c5b52c
robertlyc <merlinlyc@gmail.com>
2014-08-28 11:30:28 +0800

    add LocationViewController and set talbe Height

---
+++ MyLocations/LocationsViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocationsViewController.h
+//  MyLocations
+//
+//  Created by RoBeRt on 14-8-28.
+//  Copyright (c) 2014年 SpringShine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationsViewController : UITableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ MyLocations/LocationsViewController.m
@@ -0,0 +1,55 @@
+//
+//  LocationsViewController.m
+//  MyLocations
+//
+//  Created by RoBeRt on 14-8-28.
+//  Copyright (c) 2014年 SpringShine. All rights reserved.
+//
+
+#import "LocationsViewController.h"
+
+@interface LocationsViewController ()
+
+@end
+
+@implementation LocationsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Location"];
+    
+    UILabel *descriptionLabel = (UILabel *)[cell viewWithTag:100];
+    descriptionLabel.text = @"If you can see this";
+    
+    UILabel *addressLabel = (UILabel *)[cell viewWithTag:101];
+    addressLabel.text = @"Then it works!";
+    
+    return cell;
+}
+
+@end
