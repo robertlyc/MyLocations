fa5b0cff5d14713068870dc8b6991be7e75006a0
robertlyc <merlinlyc@gmail.com>
2014-09-02 15:04:04 +0800

    add information icon

--- MyLocations/MapViewController.m
+++ MyLocations/MapViewController.m
@@ -31,7 +31,44 @@
 }
 
 - (IBAction)showLocations:(id)sender {
+
+}
+
+- (MKCoordinateRegion)regionForAnnotations:(NSArray *)annotations {
+    MKCoordinateRegion region;
+    if ([annotations count] == 0) {
+        region = MKCoordinateRegionMakeWithDistance(self.mapView.userLocation.coordinate, 1000, 1000);
+        
+    } else if ([annotations count] == 1) {
+        id <MKAnnotation> annotation = [annotations lastObject];
+        region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 1000, 1000);
+    } else {
+        CLLocationCoordinate2D topLeftCoord;
+        topLeftCoord.latitude = -90;
+        topLeftCoord.longitude = 180;
+        
+        CLLocationCoordinate2D bottomRightCoord;
+        bottomRightCoord.latitude = 90;
+        bottomRightCoord.longitude = -180;
+        
+        for (id <MKAnnotation> annotation in annotations) {
+            topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
+            topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
+            
+            bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
+            bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
+        }
+        
+        const double extraSpace = 1.1;
+        
+        region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude -bottomRightCoord.latitude) / 2.0;
+        region.center.longitude = topLeftCoord.longitude - (topLeftCoord.longitude -bottomRightCoord.longitude) / 2.0;
+        
+        region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * extraSpace;
+        region.span.longitudeDelta = fabs(topLeftCoord.longitude - bottomRightCoord.longitude) * extraSpace;
+    }
     
+    return [self.mapView regionThatFits:region];
 }
 
 - (void)updateLocations {
@@ -60,15 +97,35 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - MKMapViewDelegate
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    if ([annotation isKindOfClass:[Location class]]) {
+        static NSString *identifier = @"Location";
+        MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+        
+        if (annotationView == nil) {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+            annotationView.enabled = YES;
+            annotationView.canShowCallout = YES;
+            annotationView.animatesDrop = YES;
+            annotationView.pinColor = MKPinAnnotationColorGreen;
+            
+            UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+            [rightButton addTarget:self action:@selector(showLocationDetails:) forControlEvents:UIControlEventTouchUpInside];
+            annotationView.rightCalloutAccessoryView = rightButton;
+        } else {
+            annotationView.annotation = annotation;
+        }
+        
+        UIButton *button = (UIButton *)annotationView.rightCalloutAccessoryView;
+        button.tag = [_locations indexOfObject:(Location *)annotation];
+        return annotationView;
+    }
+    return nil;
 }
-*/
+
+- (void)showLocationDetails:(UIButton *)button {
+    
+}
 
 @end
